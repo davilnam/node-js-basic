2e356723a56f6b069878ee01b99340d4d87e5910
Tran Van Nam <namtran10a2nt@gmail.com>
2022-09-30 08:31:59 +0700

    hello world

--- .history/server_20220930082642.js
+++ .history/server_20220930082642.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
 app.listen(port, () => {
-    console.log('Node.JS server is running on port: 8000');
+    console.log('Node.JS server is running on port: 3000');
 })
