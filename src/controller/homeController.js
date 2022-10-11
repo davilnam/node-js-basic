3e92eebe7aea63397b34c99d27014f9c6a6c6f96
Tran Van Nam <namtran10a2nt@gmail.com>
2022-10-11 19:01:50 +0700

    crud and rest api

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,28 +1,49 @@
-import { application } from 'express';
 import pool from '../configs/connectDB';
 
 let getHomePage = async (req, res) => {
     const [rows, fields] = await pool.execute('SELECT * FROM `users`');
+    console.log(typeof rows);
     return res.render("index.ejs", { dataUser: rows });
 }
 
 let getDatailPage = async (req, res) => {
     let id = req.params.userId;
     let [user] = await pool.execute('SELECT * FROM `users` WHERE id = ?', [id]);
-    console.log(typeof user);
+    // console.log(typeof user);
     return res.send(user);
 }
 
 let createNewUser = async (req, res) => {
     let { firstName, lastName, email, address } = req.body;
+    console.log(req.body);
     await pool.execute('INSERT INTO `users` (firstName, lastName, email, address) VALUES (?, ?, ?, ?)',
         [firstName, lastName, email, address]);
     return res.redirect('/');
 }
 
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    await pool.execute('delete from `users` where id = ?', [userId]);
+    return res.redirect('/');
+}
+
+let getEditPage = async (req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('SELECT * FROM `users` WHERE id = ?', [id]);
+    return res.render("update.ejs", { dataUser: user[0] });
+}
+
+let postUpdateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+    await pool.execute('update `users` set firstName = ?, lastName = ?, email = ?, address = ?  where id = ?',
+        [firstName, lastName, email, address, id]);
+    return res.redirect('/');
+}
 
 module.exports = {
     getHomePage,
     getDatailPage,
-    createNewUser
+    createNewUser,
+    deleteUser, getEditPage, postUpdateUser
 }
+
--- src/route/web.js
+++ src/route/web.js
@@ -6,11 +6,10 @@ let router = express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/detail/user/:userId', homeController.getDatailPage);
-    router.post('/create-new-user', homeController.createNewUser)
-    router.get('/about', (req, res) => {
-        res.send("I'm Nam");
-    })
-
+    router.post('/create-new-user', homeController.createNewUser);
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.getEditPage);
+    router.post('/update-user', homeController.postUpdateUser);
     return app.use('/', router);
 }
 
